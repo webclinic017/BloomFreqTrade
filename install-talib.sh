dabdbfb96ec5f82ab9dda243bd91cbf1525e4aa0
Mohsen Hassan <35639658+MTG-Mohsen@users.noreply.github.com>
2018-11-26 15:42:00 -0500

    Delete install-talib.sh

--- install-talib.sh
+++
@@ -1,8 +0,0 @@
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-tar -xzf ta-lib-0.4.0-src.tar.gz
-cd ta-lib/
-./configure
-make
-sudo make install
-pip3 install numpy
-pip3 install TA-Lib
